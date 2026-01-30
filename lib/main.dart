c24369036137541cb3bf4550408539ce9c29b942
John Suyang <johnsuyang@gmail.com>
2026-01-30 12:02:30 +0800

    setup supabase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutterblognew/router.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() => runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: "/.env");
+  await Supabase.initialize(
+    url: dotenv.env['SUPABASE_URL']!,
+    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
+  );
+
+  return runApp(const MyApp());
+}
 
 // The main app.
 class MyApp extends StatelessWidget {
